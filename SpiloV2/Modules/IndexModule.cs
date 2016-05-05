744a98315fbbed3e3fed9f30c213f9dd91db957d
kjartanp <kjartan_palsson@hotmail.com>
2016-05-05 20:19:34 +0200

    Add project files.

---
+++ SpiloV2/Modules/IndexModule.cs
@@ -0,0 +1,15 @@
+﻿using Nancy;
+
+namespace SpiloV2.Modules
+{
+	public class IndexModule : NancyModule
+	{
+		public IndexModule()
+		{
+			Get["/"] = parameters =>
+			{
+				return View["index"];
+			};
+		}
+	}
+}
